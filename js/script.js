c776a2dba7ff3f055965b3f540c0c6a1749f5057
Akshat <akshatsahu100@gmail.com>
2021-07-11 22:34:19 +0530

    code cleanup

--- js/script.js
+++ js/script.js
@@ -1,80 +1,35 @@
-let drums = new Howl({
-     src: ['./audio/piano.mp3']
-});
-let guitar = new Howl({
-     src: ['./audio/guitar.mp3']
-});
-let mic = new Howl({
-     src: ['./audio/mic.mp3']
-});
-let piano = new Howl({
-     src: ['./audio/piano.mp3']
-});
-
-drums.play();
-guitar.play();
-mic.play();
-piano.play();
-
-function add(event){
-     let data = event.target.data;
-     console.log(data);
-     data.mute(false);
-}
-function addDrums() {
-     drums.mute(false);
-}
-function addGuitar() {
-     guitar.mute(false);
-}
-function addMic() {
-     mic.mute(false);
-}
-function addPiano() {
-     piano.mute(false);
-}
-function removeDrums() {
-     drums.mute(true);
-}
-function removePiano() {
-     piano.mute(true);
-}
-function removeMic() {
-     mic.mute(true);
-}
-function removeGuitar() {
-     guitar.mute(true);
-}
 window.onload = function () {
+     let drums = new Howl({
+          src: ['./audio/piano.mp3']
+     });
+     let guitar = new Howl({
+          src: ['./audio/guitar.mp3']
+     });
+     let mic = new Howl({
+          src: ['./audio/mic.mp3']
+     });
+     let piano = new Howl({
+          src: ['./audio/piano.mp3']
+     });
+     
+     drums.play();
+     guitar.play();
+     mic.play();
+     piano.play();
+     
      const drumsMarker = document.getElementById('drums-marker');
-     drumsMarker.addEventListener('markerFound', function () {
-          addDrums();
-     });
-     drumsMarker.addEventListener('markerLost', function () {
-          removeDrums();
-     });
+     drumsMarker.addEventListener('markerFound', () => drums.mute(false));
+     drumsMarker.addEventListener('markerLost', () => drums.mute(true));
 
      const pianoMarker = document.getElementById('piano-marker');
-     pianoMarker.addEventListener('markerFound', function () {
-          addPiano();
-     });
-     pianoMarker.addEventListener('markerLost', function () {
-          removePiano();
-     });
+     pianoMarker.addEventListener('markerFound', () => piano.mute(false));
+     pianoMarker.addEventListener('markerLost', () => piano.mute(true));
 
      const micMarker = document.getElementById('mic-marker');
-     micMarker.addEventListener('markerFound', function () {
-          addMic();
-     });
-     micMarker.addEventListener('markerLost', function () {
-          removeMic();
-     });
+     micMarker.addEventListener('markerFound',() => mic.mute(false));
+     micMarker.addEventListener('markerLost', () => mic.mute(true));
 
      const guitarMarker = document.getElementById('guitar-marker');
-     guitarMarker.addEventListener('markerFound', function () {
-          addGuitar();
-     });
-     guitarMarker.addEventListener('markerLost', function () {
-          removeGuitar();
-     });
+     guitarMarker.addEventListener('markerFound', () => guitar.mute(false));
+     guitarMarker.addEventListener('markerLost', () => guitar.mute(true));
 }
